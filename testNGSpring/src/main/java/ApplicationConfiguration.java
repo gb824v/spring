9c87c268c78f3b393416f99a18744d971ec6da8a
gb824v <gb824v@intl.att.com>
2018-06-10 17:54:01 +0300

    Commit

--- testNGSpring/src/main/java/ApplicationConfiguration.java
+++ testNGSpring/src/main/java/ApplicationConfiguration.java
@@ -1,8 +1,7 @@
-package com.javacodegeeks.testng.spring.reset;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
 @Configuration
 @PropertySource("classpath:application.properties")
 public class ApplicationConfiguration {
--- testNGSpring/src/test/java/com/javacodegeeks/testng/spring/reset/ResetAssureTest.java
+++ testNGSpring/src/test/java/com/javacodegeeks/testng/spring/reset/ResetAssureTest.java
@@ -15,85 +15,13 @@ import static io.restassured.RestAssured.given;
 public class ResetAssureTest extends AbstractTestNGSpringContextTests {
     @Autowired
     ApplicationConfiguration conf;
+    @Autowired
+    TGApi tgapi;
     private String accesstoken = null;
 
     @BeforeClass
     public void setUp() {
-        // downloadngo.fc.TestManager.loadGlobalParameters(); // NOTE: Unremark if you want to bypass suite and run just this class
-        // 1. Call TGuard:
-        System.out.println("Calling Call TGuard...");
-        String protocol = "https";
-        String TGuard_Host = "tstagesms.stage.att.net";
-        String TGuard_Path = "commonLogin/nxsATS/TokenGen";
-        String userPassword = "test1ng";
-        String appId1 = "aGm70P8TdIZ%2FluduHKmrYwcyVgU%3D";
-        //String appId = "F8LZ2DSQLX6RDIHDStbn5Vy8oAg=";
-        String userId = "qayslid_021417424079302270240";
 
-        // code that will be invoked when this test is instantiated
-        // here goes authentication
-        String url = "{{protocol}}://{{TGuard_Host}}/{{TGuard_Path}}?password={{userPassword}}&client_type=TVOS&appID={{appId}}&client_version=f7d7503c-12da-480e-9e6e-4f1012208fda%252F22&respType=json&rememberMe=on&rememberID=on&TG_OP=TokenGen&userid={{userId}}";
-
-        url = url.replace("{{protocol}}", protocol);
-        url = url.replace("{{TGuard_Host}}", TGuard_Host);
-        url = url.replace("{{TGuard_Path}}", TGuard_Path);
-        url = url.replace("{{userPassword}}", userPassword);
-        url = url.replace("{{appId}}", conf.getAppid());
-        url = url.replace("{{userId}}", userId);
-
-        Response res = given().header("Content-Type", "application/x-www-form-urlencoded").when().post(url);
-
-        //Response res = RestAssured.post(url);
-        Assert.assertEquals(200, res.getStatusCode());
-
-        String json = res.asString();
-        JsonPath jp = new JsonPath(json);
-
-        String atsToken = jp.get("atsToken");
-        Assert.assertNotNull(atsToken); // null when property is missing
-        Assert.assertNotEquals(atsToken, "");
-
-        System.out.println("Successfully received a TGuard token " + atsToken);
-        // String tToken = atsToken;
-
-        // 2: Call AuthN (authn-token-ios):
-        System.out.println("Calling AuthN...");
-
-        // {{protocol}}://{{apiHost}}/{{AuthNURL}}
-        //String apiHost = "api-dev.aeg.cloud";
-        //String envUrl = "api-stage.aeg.cloud";
-        String authNUrl = "https://{{envUrl}}/authn-token?reqParams=AUTHGROUPS";
-        authNUrl = authNUrl.replace("{{envUrl}}", conf.getAuthn_url());
-
-        String client_id = "iOS";
-        String deviceClassId = "8B8FA169-A5F0-4EAF-856A-F42403AAD41F";
-        String siteUserId = "dotcomsqa11@gmail.com";
-        String authNContent = "deviceClassId={{deviceClassId}}&client_id={{client_id}}&siteUserId={{siteUserId}}&tToken={{atsToken}}";
-        authNContent = authNContent.replace("{{client_id}}", client_id);
-        authNContent = authNContent.replace("{{deviceClassId}}", deviceClassId);
-        authNContent = authNContent.replace("{{siteUserId}}", siteUserId);
-        authNContent = authNContent.replace("{{authNContent}}", authNContent);
-        authNContent = authNContent.replace("{{atsToken}}", atsToken);
-
-        authNUrl += "?" + authNContent;
-
-        Response res1 = given().header("Content-Type", "application/x-www-form-urlencoded").when().post(authNUrl);
-
-        //Response res = RestAssured.post(url);
-        Assert.assertEquals(2500, res1.getStatusCode());
-
-        String json1 = res1.asString();
-        JsonPath jp1 = new JsonPath(json1);
-
-        String responseStatus = jp1.get("responseStatus.status");
-        Assert.assertEquals(responseStatus, "success");
-        String access_token = jp1.get("access_token");
-        Assert.assertNotNull(access_token); // null when property is missing
-
-        String accesstoken = "Bearer " + access_token;
-        this.accesstoken = accesstoken;
-
-        System.out.println("Successfully received an AuthN token");
     }
 
     @Test(testName = "Test success case of download and go endpoint",
